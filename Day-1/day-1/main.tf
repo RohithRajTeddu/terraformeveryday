27570503fd99edb116189fd70c5e683cf8598a4a
RohithRajTeddu <teddurohithraj@gmail.com>
2025-06-15 19:40:57 -0500

    Delete Day-1/day-1 directory

--- Day-1/day-1/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "ec2_1" {
-  ami = var.ami_id
-  instance_type =var.instance_type
-  count = var.instance_count
-  tags = {
-    Name = "ec2_developer"
-  }
-}
- 
--- Day-1/day-1/output.tf
+++
@@ -1,5 +0,0 @@
-output "ec2_public_ip" {
-value = aws_instance.ec2_1[0].public_ip
-description = "public IP of the EC2 instance"
-  
-}
